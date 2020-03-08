0f0721f47e7d401230f9bac6c0bdcf741e07f322
Kushal997-das <61356005+Kushal997-das@users.noreply.github.com>
2020-03-08 14:54:54 +0530

    program02 created
    Enter three integers (i)print the largest of the three.
                         (ii)sum of all inputs.
                          (iii)sum of non-duplicate number.

---
+++ program02.py
@@ -0,0 +1,40 @@
+#Q2.Enter three integers (i)print the largest of the three.
+                     #(ii)sum of all inputs.
+                      #(iii)sum of non-duplicate number.
+#(i):
+
+x, y, z =input("enter three numbers\n").split(" ")
+print(f"{x} {y} {z}")
+if(x>=y and x>=z):
+       print("x is the largest number among them",x)
+elif (y>=z):
+   print("y is largest number among them",y)
+else:
+       print("z is the largest number among them",z)
+       
+#(ii):
+
+x=int(input("enter your 1st number"))
+y=int(input("enter your 2nd number"))
+z=int(input("enter your 3rd number"))
+print(f"{x} {y} {z}")
+k=x+y+z
+print(k)
+
+#(iii):
+x=int(input("enter your 1st number"))
+y=int(input("enter your 2nd number"))
+z=int(input("enter your 3rd number"))
+if(x != y and y != z and x != z):
+   k=x+y+z
+   print(k)
+elif (x==y==z):
+   print('sum is=',0)
+elif (x==y):
+   print('sum is=',z)
+elif (y==z):
+   print("sum is=",x)
+elif (x==z):
+   print("sum is=",y)
+
+
