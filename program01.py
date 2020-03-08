8d53048542707e4c724fbf3404bed0180df6bd5e
Kushal997-das <61356005+Kushal997-das@users.noreply.github.com>
2020-03-08 14:45:28 +0530

    program01 is created
    Write a program that takes a number n checks whether the number is even or odd.

---
+++ program01.py
@@ -0,0 +1,9 @@
+#Q1.Write a program that takes a number n checks whether the number is even or odd.
+x=int(input("enter a number"))
+if(x%2==0):
+   print("number is even")
+else:
+   print("it is a odd number")
+Output:
+enter a number6
+number is even
