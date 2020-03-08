37a6d288ab7ca554fe502ff95cec3ec681121142
Kushal997-das <61356005+Kushal997-das@users.noreply.github.com>
2020-03-08 14:57:21 +0530

    program03 created
    Write a program to test the divisibility of a number with another number.

---
+++ program03.py
@@ -0,0 +1,8 @@
+#Q3.Write a program to test the divisibility of a number with another number.
+n=int(input("enter the number"))
+k=int(input("enter the divisible number"))
+if(n%k==0):
+   print("yes number is divisible by",k)
+else:
+   print("no number is not divisible by",k)
+print("final answer is=",n//k)
