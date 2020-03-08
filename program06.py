60f9870c3fb3644af99f91eb2c6e547757d9b48a
Kushal997-das <61356005+Kushal997-das@users.noreply.github.com>
2020-03-08 15:12:24 +0530

    program06
    Write a program that reads two numbers of an arithmetic operator & display the computed result.
    (i)enter 1&2
    op=input(enter operator[+ - * / %]:

---
+++ program06.py
@@ -0,0 +1,21 @@
+#Q 06.Write a program that reads two numbers of an arithmetic operator & display the computed result.
+#(i)enter 1&2
+#op=input(enter operator[+ - * / %]:
+num1=1
+num2=2
+print("enter which operator would you like to perform")
+op=input("enter any of these given operator +,-,*,/,%\n")
+if op=='+':
+   result=num1+num2
+elif op=='-':
+   result=num1-num2
+elif op=='*':
+   result=num1*num2
+elif op=='/':
+   result=num1/num2
+elif op=='%':
+   result=num1%num2
+else:
+   print(" press invalid operator")
+print(num1,op,num2,"=",result)
+
